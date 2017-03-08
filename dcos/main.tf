713128500adc9513a6947df358e8d3a9889ce7d5
Miguel Bernadin <Miguel.Bernadin@gmail.com>
2017-03-07 17:54:51 -0800

    Testing reattachment

--- dcos/main.tf
+++ dcos/main.tf
@@ -24,6 +24,7 @@ resource "aws_vpc" "default" {
 
   tags {
    Name = "${data.template_file.cluster-name.rendered}-vpc"
+   cluster = "${data.template_file.cluster-name.rendered}"
   } 
   
   lifecycle {
@@ -292,6 +293,12 @@ resource "aws_security_group" "private_slave" {
    }
 }
 
+# Reattach the internal ELBs to the master if they change
+resource "aws_elb_attachment" "internal-master-elb" {
+  elb      = "${aws_elb.internal-master-elb.id}"
+  instance = "${element(aws_instance.master.*.id, count.index)}"
+}
+
 # Internal Load Balancer Access
 # Mesos Master, Zookeeper, Exhibitor, Adminrouter, Marathon
 resource "aws_elb" "internal-master-elb" {
@@ -348,6 +355,12 @@ resource "aws_elb" "internal-master-elb" {
   }
 }
 
+# Reattach the public ELBs to the master if they change
+resource "aws_elb_attachment" "public-master-elb" {
+  elb      = "${aws_elb.public-master-elb.id}"
+  instance = "${element(aws_instance.master.*.id, count.index)}"
+}
+
 # Public Master Load Balancer Access
 # Adminrouter Only
 resource "aws_elb" "public-master-elb" {
@@ -382,6 +395,12 @@ resource "aws_elb" "public-master-elb" {
   lifecycle {
     ignore_changes = ["name"]
   }
+}
+
+# Reattach the public ELBs to the agents if they change
+resource "aws_elb_attachment" "public-agent-elb" {
+  elb      = "${aws_elb.public-agent-elb.id}"
+  instance = "${element(aws_instance.agent.*.id, count.index)}"
 }
 
 # Public Agent Load Balancer Access
@@ -440,7 +459,8 @@ resource "aws_instance" "master" {
   tags {
    owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
    expiration = "${var.expiration}"
-   Name = "${data.template_file.cluster-name.rendered}-master-${count.index + 1}"
+   Name = "${aws_vpc.default.tags.cluster}-master-${count.index + 1}"
+   cluster = "${aws_vpc.default.tags.cluster}"
   }
 
   # Lookup the correct AMI based on the region
@@ -475,7 +495,7 @@ resource "aws_instance" "master" {
   }
 
   lifecycle {
-    ignore_changes = ["tags.Name"]
+    ignore_changes = ["tags.Name", "tags.cluster"]
   }
 }
 
@@ -499,7 +519,8 @@ resource "aws_instance" "agent" {
   tags {
    owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
    expiration = "${var.expiration}"
-   Name =  "${data.template_file.cluster-name.rendered}-pvtagt-${count.index + 1}"
+   Name =  "${aws_vpc.default.tags.cluster}-pvtagt-${count.index + 1}"
+   cluster = "${aws_vpc.default.tags.cluster}"
   }
   # Lookup the correct AMI based on the region
   # we specified
@@ -556,7 +577,8 @@ resource "aws_instance" "bootstrap" {
   tags {
    owner = "${coalesce(var.owner, data.external.whoami.result["owner"])}"
    expiration = "${var.expiration}"
-   Name = "${data.template_file.cluster-name.rendered}-bootstrap"
+   Name = "${aws_vpc.default.tags.cluster}-bootstrap"
+   cluster = "${aws_vpc.default.tags.cluster}"
   }
 
   # Lookup the correct AMI based on the region
@@ -634,6 +656,11 @@ resource "null_resource" "bootstrap" {
 }
 
 resource "null_resource" "master" {
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    cluster_instance_ids = "${join(",", aws_instance.bootstrap.*.id)}"
+  }
+
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
@@ -658,7 +685,10 @@ resource "null_resource" "master" {
 }
 
 resource "null_resource" "agent" {
-  count = "${var.num_of_private_agents}"
+  # Changes to any instance of the cluster requires re-provisioning
+  triggers {
+    cluster_instance_ids = "${join(",", aws_instance.bootstrap.*.id)}"
+  }
 
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
@@ -666,6 +696,8 @@ resource "null_resource" "agent" {
     host = "${element(aws_instance.agent.*.public_ip, count.index)}"
     user = "${var.user}"
   }
+
+  count = "${var.num_of_private_agents}"
 
   # We run a remote provisioner on the instance after creating it.
   # In this case, we just install nginx and start it. By default,
